09c47b33fdeca458ca149a2326342c8f85993cad
sauravmondal24 <sauravmondal321@gmail.com>
2022-10-26 14:37:49 +0600

    course related data load

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-// const port = process.env.PORT || 5000;
-const post = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require('./data/categories.json');
+const categories = require('./data/dataList/categories.json');
+const course = require('./data/dataList/courseDetails.json');
 
 app.get('/', (req, res) => {
 	res.send('Server App running');
@@ -14,6 +14,27 @@ app.get('/', (req, res) => {
 
 app.get('/course-category', (req, res) => {
 	res.send(categories);
+});
+
+app.get('/category/:id', (req, res) => {
+	const id = req.params.id;
+
+	if (id === '08') {
+		res.send(course);
+	} else {
+		const categoryCourse = course.filter((n) => n.category_id === id);
+		res.send(categoryCourse);
+	}
+});
+
+app.get('/course', (req, res) => {
+	res.send(course);
+});
+
+app.get('/course/:id', (req, res) => {
+	const id = req.params.id;
+	const selectedCourse = course.find((n) => n._id === id);
+	res.send(selectedCourse);
 });
 
 app.listen(port, () => {
