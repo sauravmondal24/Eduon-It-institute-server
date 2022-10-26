90c74dd872878b2d0b2ea42f519e9d988dae1762
sauravmondal24 <sauravmondal321@gmail.com>
2022-10-26 11:08:05 +0600

    server port setup

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
+const post = process.env.PORT || 5000;
 
 app.use(cors());
 
